111d27a25d540620098cf82677525f835600dd19
a_shuusa <a.shusa0820@gmail.com>
2012-12-03 08:27:58 +0900

    コンフリクト修正

--- public/js/api.js
+++ public/js/api.js
@@ -4,34 +4,6 @@ var Api = function(apiName, param){
 	this.apiName = apiName;
 }
 
-<<<<<<< HEAD
-Api.prototype.getApiUrl = function(apiName){
-	var apiUrlMap = {
-						getFiles : "http://localhost:8888/sample/kanai/json.html"
-					}
-	return apiUrlMap[apiName];
-}
-
-Api.prototype.getFiles = function(){
-	var res = this.request();
-	return res;
-}
-
-Api.prototype.request = function(){
-	var res;
-	var url = this.getApiUrl(this.apiName);
-	$.ajax({
-	    'type': 'GET',
-	    'url': url,
-	    'async' : false,
-	    'dataType': 'json',
-	    'data': this.param,
-	    'success': function(data) {
-	    	res = data;
-	    },
-	    'error': function() {
-	    	alert('取得error');
-=======
 Api.prototype.urlMap = {
 	getFiles : "http://localhost:8888/sample/kanai/json.html"
 }
@@ -60,7 +32,6 @@ Api.prototype.request = function(){
 	    },
 	    'error': function() {
 	    	alert('error');
->>>>>>> branch 'master' of https://github.com/am8workshop/ApplyTool.git
 	   }
 	});
 	return res;
