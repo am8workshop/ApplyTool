42fa613e391e10a0b21cef270b370e9cfd437ad8
a_shuusa <a.shusa0820@gmail.com>
2012-12-17 21:38:20 +0900

    ファイルアップロードjs

--- public/sample/shuusa/FileUpload.js
+++ public/sample/shuusa/FileUpload.js
@@ -1,5 +1,5 @@
     var uploadFiles = function () {
-    	var form = document.getElementById('fileForm');
+    	var form = $('#fileForm')[0];
         var fd = new FormData(form);
         // XHR で送信
         $.ajax({
@@ -10,29 +10,3 @@
             contentType: false
         });
     };
-
-//    // ファイル選択フォームからの入力
-//    $("#form").bind("change", function () {
-//        // 選択されたファイル情報を取得
-//        var files = this.files;
-//
-//        // アップロード処理
-//        uploadFiles(files);
-//    });
-
-//    // ドラッグドロップからの入力
-//    $("#target").bind("drop", function (e) {
-//        // ドラッグされたファイル情報を取得
-//        var files = e.originalEvent.dataTransfer.files;
-//
-//        // アップロード処理
-//        uploadFiles(files);
-//    })
-//    .bind("dragenter", function () {
-//        // false を返してデフォルトの処理を実行しないようにする
-//        return false;
-//    })
-//    .bind("dragover", function () {
-//        // false を返してデフォルトの処理を実行しないようにする
-//        return false;
-//    });
